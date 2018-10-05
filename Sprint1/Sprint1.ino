1ca5b58d09ffa223573679ab1c61be035948d9a8
Alan Smith <alan@randomsmiths.com>
2018-10-05 10:26:41 -0400

    cleanup - no real functionality change

--- Sprint1/Sprint1.ino
+++ Sprint1/Sprint1.ino
@@ -2,16 +2,16 @@
 #include <NewPing.h>
 #include <LiquidCrystal.h>
 
-int reedPin = A0;       // select the input  pin for  the reed
-int TRIGGER_PIN = 4;    // Arduino pin tied to trigger pin on the ultrasonic sensor.
-int ECHO_PIN = 6;       // Arduino pin tied to echo pin on the ultrasonic sensor.
 int MAX_DISTANCE = 400; // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 int BLAST_ZONE = 10;    // Closest distance we can get to the end zone
-int sensorPin = A1;     // select the input  pin for  the potentiometer
-int sensorValue = 0;    // variable to  store  the value  coming  from  the sensor
-int reedValue = 0;      // variable to  store  the value  coming  from  the sensor
 
-Boolean lookingForBand = true;
+// Pin defines
+const int REED_PIN = A0;         // select the input  pin for  the reed
+const int PHOTO_SENSOR_PIN = A1; // select the input  pin for  the potentiometer
+const int TRIGGER_PIN = 4;       // Arduino pin tied to trigger pin on the ultrasonic sensor.
+const int ECHO_PIN = 6;          // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+bool lookingForBand = true;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 Servo LeftServo;                                    // create new servo object for the left servo
@@ -53,80 +53,61 @@ void setup()
   // Initialize the Serial monitor for readings from the robot
   Serial.begin(9600);
 
-  stop();
+  stopWheels();
 }
 
 void loop()
 {
+  int photoSensor = analogRead(PHOTO_SENSOR_PIN);
+  int reedValue = analogRead(REED_PIN);
 
-  // read the potentiometer on A1:
-  //int sensorReading = analogRead(A1);
-  // set cursor at the start of line 2
-  lcd.setCursor(byte(0), 1);
+  int cm_away = sonar.convert_cm(sonar.ping_median());
 
-  /*
-  LeftServo.write(180); // Run Left servo full reverse
-  RightServo.write(0);  //Run Right servo full forward
-  delay(2000);
-*/
-
-  sensorValue = analogRead(sensorPin);
-  reedValue = analogRead(reedPin);
-
-  int cm_away = getDistance();
-
-  send_serial_data(cm_away, sensorValue, reedValue);
+  send_serial_data(cm_away, photoSensor, reedValue);
+  update_display(cm_away, photoSensor, reedValue);
 
   if (cm_away < BLAST_ZONE)
   {
-    stop();
+    stopWheels();
   }
   else
   {
-    drive();
+    driveFullSpeed();
   }
-
-  //send the readings to the LCD screen
-  lcd.print(cm_away);
-  lcd.print("cm");
-  lcd.print("/");
-  lcd.print(sensorValue); // shows reading off of photoresistor
-  lcd.print("/");
-  lcd.print(reedValue);
-
-  /*
-  //Stop the servo for 2000 milliseconds
-  LeftServo.write(90);
-  RightServo.write(90);
-  delay(2000);
-*/
 }
 
-void send_serial_data(int distance, int sensorValue, int reedValue)
+void update_display(int distance, int photoSensor, int reedValue)
+{
+  // set cursor at the start of line 2
+  lcd.setCursor(byte(0), 1);
+  //send the readings to the LCD screen
+  lcd.print(distance);
+  lcd.print("cm");
+  lcd.print("/");
+  lcd.print(photoSensor); // shows reading off of photoresistor
+  lcd.print("/");
+  lcd.print(reedValue);
+}
+
+void send_serial_data(int distance, int photoSensor, int reedValue)
 {
   // send the reading to the serial monitor
   Serial.print("Ping: ");
   Serial.print(distance); // Convert ping time to distance in cm and print result (0 = outside set distance range)
   Serial.println("cm");
-  Serial.println(sensorValue); // shows reading off of photoresistor
+  Serial.println(photoSensor); // shows reading off of photoresistor
   Serial.print("Reed: ");
   Serial.println(reedValue); // show reed value
 }
 
-void drive()
+void driveFullSpeed()
 {
   LeftServo.write(180); // Run Left servo full reverse
   RightServo.write(0);  //Run Right servo full forward
 }
 
-void stop()
+void stopWheels()
 {
   LeftServo.write(90);  // Stop Left servo
   RightServo.write(90); // Stop Right servo
-}
-
-int getDistance()
-{
-  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
-  return uS / US_ROUNDTRIP_CM;
 }
